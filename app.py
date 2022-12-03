9a166abb2cb328e24ed07c8c82fbd2c5f1d5a669
Rahul Mula <96373135+mulaRahul@users.noreply.github.com>
2022-12-03 12:23:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-# write your code here 👇
+is_negative = lambda num: num < 0
+is_even = lambda num: num%2 == 0
+
+def is_prime(num):
+    if is_even(num) and num == 2:
+        return False
+        
+    for x in range(3, num, 2):
+        if num%x == 0:
+            return False
+
+    return True
+    
+
+# tests
+print(f"{is_negative(-3) = }")
+print(f"{is_even(9) = }")
+print(f"{is_prime(17) = }")
