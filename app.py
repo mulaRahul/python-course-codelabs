51ce8da112e26e872a3fb3ea02cdc8636d5e8520
Rahul Mula <96373135+mulaRahul@users.noreply.github.com>
2022-12-03 12:30:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-# write your code here 👇
+# declare the classes here 👇
+
+
+
+# test code
+py_course = Course("Python")
+
+py_course.add_student(Student("Dale", 27))
+py_course.add_student(Student("Hary", 33))
+py_course.add_student(Student("John", 21))
