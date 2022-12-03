6384f77546cb3aa9df64b5df2498dbf890ec27fb
Rahul Mula <96373135+mulaRahul@users.noreply.github.com>
2022-12-03 11:48:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
-lst = ["10 Min", "5 Min", "2 Min", "9 Min", "14 Min"]
+# data
+store = {
+#    name     price
+    "Muffin": 6.8, 
+    "Cake":   11.2,
+    "Chips":  1.4, 
+    "Egg":    2.8,
+    "Bread":  1.2, 
+    "Milk":   2.6,
+    "Curd":   4.5, 
+    "Egg":    2.8,
+}
+
+purchased = {
+#   name    quantity 
+    "Egg":     4,
+    "Bread":   2,
+    "Curd":    3,
+    "Cake":    1,
+    "Muffin":  6,
+}
+
+# keep paitence
