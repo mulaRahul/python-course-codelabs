d82d3e58d2db10b19adc79d7e09a0abd3fd4e57b
Rahul Mula <96373135+mulaRahul@users.noreply.github.com>
2022-12-03 09:51:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-# write your code here 👇
+product_items = {
+#   Title      Price    
+    "Cookies": 5, 
+    "Cake":    12.5, 
+    "Muffin":  0.8, 
+    "Can":     1.4,
+}
+
+# Helper variables
+total = 0
+product_names = ['Cookies', 'Cake', 'Muffin', 'Can']
+
+# 👇 Find the total here...
+
+
+# Printing the total
+print(total)
